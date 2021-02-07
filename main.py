8728be757ca26369dd07789b3028c1321a7c3400
Siddharth Desai <siddharth.desai1996@gmail.com>
2021-02-08 00:01:26 +0530

    GUI basics to remember

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+
+def button_clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+
+
 #Window
 window = Tk()
 window.title("My First GUI Program")
@@ -7,25 +13,24 @@ window.minsize(width=500, height=300)
 
 #Label
 my_label = Label(text="I am a Label", font=("Arial", 24, "bold"))
-my_label.pack(side="top")
+#my_label.pack(side="top")
+#my_label.place(x=150,y=0)
+my_label.grid(column=0, row=0)
 
+button = Button(text="New Button", command=button_clicked)
+#button.pack()
+button.grid(column=2,row=0)
 
 #button
-def button_clicked():
-    new_text = input.get()
-    my_label.config(text=new_text)
-
-
 button = Button(text="Click me", command=button_clicked)
-button.pack()
+#button.pack()
+button.grid(column=1,row=1)
 
 #Entry
-
-input = Entry()
-ent = input.pack()
-button = Button(text="Click Me", command=button_clicked)
-button.pack()
-
+input = Entry(width=10)
+input.get()
+#input.pack()
+input.grid(column=1 , row=2)
 
 
 
